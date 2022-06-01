0a2f1c4338541d0f317f1964d822b0de0a8f6178
Joseph Yika <josephyika123@gmail.com>
2022-06-01 11:37:43 +0200

    Configured Default c# file
    Configured default c# .txt file. It is the file that opens a default c# file.
    
    We can change it by navigating to Program Files -> Unity - Resources - Templates and the very first file is the one that we can change.

---
+++ MusicalGame/Assets/PlayerKeyMovement.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerKeyMovement : MonoBehaviour
+{
+    /*Start is called before the first frame update. 
+      This method is called at the start of the game - hence the name*/
+
+    void Start()
+    {
+
+        Debug.Log("Hello, World");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
